aab8eca9c293d2e938019c02b7b499e957f9446b
RobbinGIT <robbin.githimbo@student.moringaschool.com>
2021-11-15 01:14:43 +0300

    add title

--- app/auth/views.py
+++ app/auth/views.py
@@ -21,7 +21,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "Pinblog"
+    title = "TheBlog"
     return render_template('auth/login.html',
                            login_form=login_form,
                            title=title)
